ec282477f900a3884aaa40fb17ec3511c8a5f1bc
Obed Lorisson <obed.lorisson@gmail.com>
2018-03-02 15:14:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -6,5 +6,5 @@ def line(array)
    if array.empty?
      puts "The line is currently empty."
    else
-     array.each do {|i| puts 
+     array.each_index do {|i| puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
 end
