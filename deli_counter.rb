f0908d6252f18b07e0f28bf0f412ed6efe142114
Obed Lorisson <obed.lorisson@gmail.com>
2018-03-02 15:27:08 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,11 @@
 # Write your code here.
 
-katz_deli =[]
-
 def line(array)
    if array.empty?
      puts "The line is currently empty."
    else
-     array.each_index do {|i| puts "The line is currently: 1. Logan 2. Avi 3. Spencer"
+     array.each do |i|
+       puts "The line is currently: #{array.index(i)} #{i}"
+     end
+   end
 end
